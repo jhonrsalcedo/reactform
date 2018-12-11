a29aeab86f47e3e936a60f2fa3a6653e1152f958
jhonrsalcedo <jhonfor@gmail.com>
2018-12-11 16:31:45 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4>Formulario</h4>
+        <div>
+          <label>Name:</label>
+          <input type='text'/>
+          <label>Apellido:</label>
+          <input type='text'/>
+        </div>
       </div>
     );
   }
