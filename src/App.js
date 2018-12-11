ec1f7ad1004836cd56e71554a20b5de6fd60fa63
jhonrsalcedo <jhonfor@gmail.com>
2018-12-11 16:53:41 -0500

    mod state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { timingSafeEqual } from 'crypto';
 
 class App extends Component {
+   constructor(){
+     super();
+     this.state ={
+       name: '',
+       lastname: ''
+     }
+   }
+   handleName = (changeName) => {
+     this.setState({
+       name: changeName.target.value
+     });
+   }
+   changeLastName = (changeLastName) => {
+     this.setState.bind({
+       lastname: changeLastName.target.value
+     });
+   }
+
   render() {
     return (
       <div className="App">
         <h4>Formulario</h4>
         <div>
-          <label>Name:</label>
-          <input type='text'/>
-          <label>Apellido:</label>
-          <input type='text'/>
+          <label>Name: </label>
+          <input type='text' onChange={changeName => this.handleName(changeName)}/> &nbsp;
+          <label>Apellido: </label>
+          <input type='text' onChange={changeLastName => this.handleLastName(changeLastName)}/>
+        </div>
+        <div>
+          {this.state.name} {this.state.lastname}
         </div>
       </div>
     );
